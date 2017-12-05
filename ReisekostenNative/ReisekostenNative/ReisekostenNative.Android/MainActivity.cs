0ac246c9598f9bf9c4c936e853145aa16edd0f72
domdop <domdop@users.noreply.github.com>
2017-12-05 13:41:26 +0100

    Neue Activity fuer die Belegliste

--- ReisekostenNative/ReisekostenNative/ReisekostenNative.Android/MainActivity.cs
+++ ReisekostenNative/ReisekostenNative/ReisekostenNative.Android/MainActivity.cs
@@ -9,10 +9,9 @@ using Android.OS;
 
 namespace ReisekostenNative.Droid
 {
-	[Activity (Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Theme.AppCompat.Light")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -21,12 +20,20 @@ namespace ReisekostenNative.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+
+			Button login = FindViewById<Button> (Resource.Id.bt_login);
+            EditText benutzer = FindViewById<EditText>(Resource.Id.et_benutzer);
+
+
+            login.Click += delegate {
+                string name = benutzer.Text.ToString();
+                if (name != null && name.Length != 0)
+                {
+                    Intent intent = new Intent(this, typeof(BelegListe));
+                    intent.PutExtra("USER", name);
+                    StartActivity(intent);
+                }
+				
 			};
 		}
 	}
