2e24fe1f94e9d0a3e2eb025f54376c7bcae8cbca
Dominik Doppler <dominik.doppler@r-software.at>
2017-12-05 19:34:14 +0100

    ios Screens

--- ReisekostenNative/ReisekostenNative/ReisekostenNative.iOS/BelegNeuTableViewController.designer.cs
+++ ReisekostenNative/ReisekostenNative/ReisekostenNative.iOS/BelegNeuTableViewController.designer.cs
@@ -9,12 +9,93 @@ using System.CodeDom.Compiler;
 
 namespace ReisekostenNative.iOS
 {
-    [Register ("BelegNeuTableViewController")]
-    partial class BelegNeuTableViewController
+	[Register ("BelegNeuTableViewController")]
+	partial class BelegNeuTableViewController
 	{
+		[Outlet]
+		UIKit.UIPickerView art { get; set; }
+
+		[Outlet]
+		UIKit.UILabel artValue { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView beleg { get; set; }
+
+		[Outlet]
+		UIKit.UITextView beschreibung { get; set; }
+
+		[Outlet]
+		UIKit.UITextField betrag { get; set; }
+
+		[Outlet]
+		UIKit.UITextField bezeichnung { get; set; }
+
+		[Outlet]
+		UIKit.UIDatePicker datum { get; set; }
+
+		[Outlet]
+		UIKit.UILabel datumValue { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lfd { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView status { get; set; }
+
+		[Action ("belegButtonPerformed:")]
+		partial void belegButtonPerformed (Foundation.NSObject sender);
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (lfd != null) {
+				lfd.Dispose ();
+				lfd = null;
+			}
+
+			if (datumValue != null) {
+				datumValue.Dispose ();
+				datumValue = null;
+			}
+
+			if (datum != null) {
+				datum.Dispose ();
+				datum = null;
+			}
+
+			if (artValue != null) {
+				artValue.Dispose ();
+				artValue = null;
+			}
+
+			if (art != null) {
+				art.Dispose ();
+				art = null;
+			}
+
+			if (betrag != null) {
+				betrag.Dispose ();
+				betrag = null;
+			}
+
+			if (status != null) {
+				status.Dispose ();
+				status = null;
+			}
+
+			if (bezeichnung != null) {
+				bezeichnung.Dispose ();
+				bezeichnung = null;
+			}
+
+			if (beschreibung != null) {
+				beschreibung.Dispose ();
+				beschreibung = null;
+			}
+
+			if (beleg != null) {
+				beleg.Dispose ();
+				beleg = null;
+			}
 		}
 	}
 }
--- ReisekostenNative/ReisekostenNative/ReisekostenNative.iOS/BelegNeuViewController.designer.cs
+++ ReisekostenNative/ReisekostenNative/ReisekostenNative.iOS/BelegNeuViewController.designer.cs
@@ -1,20 +1,85 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace ReisekostenNative.iOS
 {
-    [Register ("BelegNeuViewController")]
-    partial class BelegNeuViewController
-    {
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+	[Register ("BelegNeuViewController")]
+	partial class BelegNeuViewController
+	{
+		[Outlet]
+		UIKit.UIPickerView art { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView beleg { get; set; }
+
+		[Outlet]
+		UIKit.UITextField beschreibung { get; set; }
+
+		[Outlet]
+		UIKit.UITextField betrag { get; set; }
+
+		[Outlet]
+		UIKit.UITextField bezeichnung { get; set; }
+
+		[Outlet]
+		UIKit.UIDatePicker datum { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lfd { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView status { get; set; }
+
+		[Action ("belegButtonPerformed:")]
+		partial void belegButtonPerformed (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (lfd != null) {
+				lfd.Dispose ();
+				lfd = null;
+			}
+
+			if (datum != null) {
+				datum.Dispose ();
+				datum = null;
+			}
+
+			if (art != null) {
+				art.Dispose ();
+				art = null;
+			}
+
+			if (betrag != null) {
+				betrag.Dispose ();
+				betrag = null;
+			}
+
+			if (status != null) {
+				status.Dispose ();
+				status = null;
+			}
+
+			if (bezeichnung != null) {
+				bezeichnung.Dispose ();
+				bezeichnung = null;
+			}
+
+			if (beschreibung != null) {
+				beschreibung.Dispose ();
+				beschreibung = null;
+			}
+
+			if (beleg != null) {
+				beleg.Dispose ();
+				beleg = null;
+			}
+		}
+	}
 }
