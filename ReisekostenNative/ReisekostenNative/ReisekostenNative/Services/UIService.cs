e5db73a1117a681e846703c0617869cf11219652
Gerald <zellner.gerald@gmail.com>
2017-12-05 21:44:44 +0100

    ui-svc added

---
+++ ReisekostenNative/ReisekostenNative/ReisekostenNative/Services/UIService.cs
@@ -0,0 +1,57 @@
+﻿using IO.Swagger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReisekostenNative.Services
+{
+    public class UIService
+    {
+        private static UIService instance;
+
+        private RESTClient.RESTClient client;
+
+        public static UIService Instance
+        {
+            get
+            {
+                return instance ?? (instance = new UIService());
+            }
+        }
+
+        private UIService()
+        {
+            client = new RESTClient.RESTClient();
+        }
+
+        public async void GetBelegarten(Action<Task<List<string>>> callback)
+        {
+           client.GetTypesAsync().ContinueWith((o) => callback(o));
+        }
+
+        public async void GetBelege(string user, Action<Task<List<Beleg>>> callback)
+        {
+            client.GetBelegeByUserAsync(user).ContinueWith((o) => callback(o));
+        }
+
+        public async void GetExported(string user, Action<Task<List<Beleg>>> callback)
+        {
+            this.client.GetBelegeByUserAsync(user).ContinueWith(o => callback(o));
+        }
+
+        public async void CreateBeleg(string user, Beleg beleg, Action<Task<int>> callback)
+        {
+            beleg.Date = DateTime.UtcNow.Date;
+            beleg.Status = Beleg.StatusEnum.ERFASST;
+
+            client.CreateBeleg(user, beleg).ContinueWith((o) => callback(o));
+        }
+
+        public async void UpdateBeleg(string user, int belegnummer, Beleg beleg)
+        {
+            this.client.UpdateBeleg(user, belegnummer, beleg);
+        }
+    }
+}
