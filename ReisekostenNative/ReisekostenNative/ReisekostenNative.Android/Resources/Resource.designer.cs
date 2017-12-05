254bf620622ebd63fd96f44642b8a3386082e043
domdop <domdop@users.noreply.github.com>
2017-12-05 20:15:56 +0100

    Styles aktualisiert, Belegerfassung

--- ReisekostenNative/ReisekostenNative/ReisekostenNative.Android/Resources/Resource.designer.cs
+++ ReisekostenNative/ReisekostenNative/ReisekostenNative.Android/Resources/Resource.designer.cs
@@ -2134,26 +2134,26 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02006a
-			public const int avd_hide_password_1 = 2130837610;
-			
 			// aapt resource value: 0x7f02006b
-			public const int avd_hide_password_2 = 2130837611;
+			public const int avd_hide_password_1 = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int avd_hide_password_3 = 2130837612;
+			public const int avd_hide_password_2 = 2130837612;
+			
+			// aapt resource value: 0x7f02006d
+			public const int avd_hide_password_3 = 2130837613;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02006d
-			public const int avd_show_password_1 = 2130837613;
-			
 			// aapt resource value: 0x7f02006e
-			public const int avd_show_password_2 = 2130837614;
+			public const int avd_show_password_1 = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int avd_show_password_3 = 2130837615;
+			public const int avd_show_password_2 = 2130837615;
+			
+			// aapt resource value: 0x7f020070
+			public const int avd_show_password_3 = 2130837616;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2180,43 +2180,46 @@ namespace ReisekostenNative.Droid
 			public const int Icon = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int navigation_empty_icon = 2130837597;
+			public const int navbar_logo = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int notification_action_background = 2130837598;
+			public const int navigation_empty_icon = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int notification_bg = 2130837599;
+			public const int notification_action_background = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int notification_bg_low = 2130837600;
+			public const int notification_bg = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int notification_bg_low_normal = 2130837601;
+			public const int notification_bg_low = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int notification_bg_low_pressed = 2130837602;
+			public const int notification_bg_low_normal = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int notification_bg_normal = 2130837603;
+			public const int notification_bg_low_pressed = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int notification_bg_normal_pressed = 2130837604;
+			public const int notification_bg_normal = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int notification_icon_background = 2130837605;
-			
-			// aapt resource value: 0x7f020068
-			public const int notification_template_icon_bg = 2130837608;
-			
-			// aapt resource value: 0x7f020069
-			public const int notification_template_icon_low_bg = 2130837609;
+			public const int notification_bg_normal_pressed = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int notification_tile_bg = 2130837606;
+			public const int notification_icon_background = 2130837606;
+			
+			// aapt resource value: 0x7f020069
+			public const int notification_template_icon_bg = 2130837609;
+			
+			// aapt resource value: 0x7f02006a
+			public const int notification_template_icon_low_bg = 2130837610;
 			
 			// aapt resource value: 0x7f020067
-			public const int notify_panel_notification_icon_bg = 2130837607;
+			public const int notification_tile_bg = 2130837607;
+			
+			// aapt resource value: 0x7f020068
+			public const int notify_panel_notification_icon_bg = 2130837608;
 			
 			static Drawable()
 			{
@@ -2231,8 +2234,8 @@ namespace ReisekostenNative.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f07008c
-			public const int action0 = 2131165324;
+			// aapt resource value: 0x7f07008e
+			public const int action0 = 2131165326;
 			
 			// aapt resource value: 0x7f070064
 			public const int action_bar = 2131165284;
@@ -2255,17 +2258,17 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070041
 			public const int action_bar_title = 2131165249;
 			
-			// aapt resource value: 0x7f070089
-			public const int action_container = 2131165321;
+			// aapt resource value: 0x7f07008b
+			public const int action_container = 2131165323;
 			
 			// aapt resource value: 0x7f070065
 			public const int action_context_bar = 2131165285;
 			
-			// aapt resource value: 0x7f070090
-			public const int action_divider = 2131165328;
+			// aapt resource value: 0x7f070092
+			public const int action_divider = 2131165330;
 			
-			// aapt resource value: 0x7f07008a
-			public const int action_image = 2131165322;
+			// aapt resource value: 0x7f07008c
+			public const int action_image = 2131165324;
 			
 			// aapt resource value: 0x7f070003
 			public const int action_menu_divider = 2131165187;
@@ -2282,11 +2285,11 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070043
 			public const int action_mode_close_button = 2131165251;
 			
-			// aapt resource value: 0x7f07008b
-			public const int action_text = 2131165323;
+			// aapt resource value: 0x7f07008d
+			public const int action_text = 2131165325;
 			
-			// aapt resource value: 0x7f070099
-			public const int actions = 2131165337;
+			// aapt resource value: 0x7f07009b
+			public const int actions = 2131165339;
 			
 			// aapt resource value: 0x7f070044
 			public const int activity_chooser_view_content = 2131165252;
@@ -2318,14 +2321,14 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070028
 			public const int bottom = 2131165224;
 			
-			// aapt resource value: 0x7f070088
-			public const int bt_login = 2131165320;
+			// aapt resource value: 0x7f07008a
+			public const int bt_login = 2131165322;
 			
 			// aapt resource value: 0x7f07004b
 			public const int buttonPanel = 2131165259;
 			
-			// aapt resource value: 0x7f07008d
-			public const int cancel_action = 2131165325;
+			// aapt resource value: 0x7f07008f
+			public const int cancel_action = 2131165327;
 			
 			// aapt resource value: 0x7f070030
 			public const int center = 2131165232;
@@ -2339,8 +2342,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f07005b
 			public const int checkbox = 2131165275;
 			
-			// aapt resource value: 0x7f070095
-			public const int chronometer = 2131165333;
+			// aapt resource value: 0x7f070097
+			public const int chronometer = 2131165335;
 			
 			// aapt resource value: 0x7f070039
 			public const int clip_horizontal = 2131165241;
@@ -2351,14 +2354,14 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070024
 			public const int collapseActionView = 2131165220;
 			
-			// aapt resource value: 0x7f07007b
-			public const int container = 2131165307;
+			// aapt resource value: 0x7f07007d
+			public const int container = 2131165309;
 			
 			// aapt resource value: 0x7f07004e
 			public const int contentPanel = 2131165262;
 			
-			// aapt resource value: 0x7f07007c
-			public const int coordinator = 2131165308;
+			// aapt resource value: 0x7f07007e
+			public const int coordinator = 2131165310;
 			
 			// aapt resource value: 0x7f070055
 			public const int custom = 2131165269;
@@ -2375,20 +2378,20 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070047
 			public const int default_activity_button = 2131165255;
 			
-			// aapt resource value: 0x7f07007e
-			public const int design_bottom_sheet = 2131165310;
+			// aapt resource value: 0x7f070080
+			public const int design_bottom_sheet = 2131165312;
+			
+			// aapt resource value: 0x7f070087
+			public const int design_menu_item_action_area = 2131165319;
+			
+			// aapt resource value: 0x7f070086
+			public const int design_menu_item_action_area_stub = 2131165318;
 			
 			// aapt resource value: 0x7f070085
-			public const int design_menu_item_action_area = 2131165317;
+			public const int design_menu_item_text = 2131165317;
 			
 			// aapt resource value: 0x7f070084
-			public const int design_menu_item_action_area_stub = 2131165316;
-			
-			// aapt resource value: 0x7f070083
-			public const int design_menu_item_text = 2131165315;
-			
-			// aapt resource value: 0x7f070082
-			public const int design_navigation_view = 2131165314;
+			public const int design_navigation_view = 2131165316;
 			
 			// aapt resource value: 0x7f070012
 			public const int disableHome = 2131165202;
@@ -2399,8 +2402,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070021
 			public const int end = 2131165217;
 			
-			// aapt resource value: 0x7f07009f
-			public const int end_padder = 2131165343;
+			// aapt resource value: 0x7f0700a1
+			public const int end_padder = 2131165345;
 			
 			// aapt resource value: 0x7f07002a
 			public const int enterAlways = 2131165226;
@@ -2408,8 +2411,14 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f07002b
 			public const int enterAlwaysCollapsed = 2131165227;
 			
-			// aapt resource value: 0x7f070087
-			public const int et_benutzer = 2131165319;
+			// aapt resource value: 0x7f070089
+			public const int et_benutzer = 2131165321;
+			
+			// aapt resource value: 0x7f070077
+			public const int et_bezeichnung = 2131165303;
+			
+			// aapt resource value: 0x7f070076
+			public const int et_date = 2131165302;
 			
 			// aapt resource value: 0x7f07002c
 			public const int exitUntilCollapsed = 2131165228;
@@ -2420,8 +2429,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f07005a
 			public const int expanded_menu = 2131165274;
 			
-			// aapt resource value: 0x7f070078
-			public const int fab_add = 2131165304;
+			// aapt resource value: 0x7f07007a
+			public const int fab_add = 2131165306;
 			
 			// aapt resource value: 0x7f07003b
 			public const int fill = 2131165243;
@@ -2444,8 +2453,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070049
 			public const int icon = 2131165257;
 			
-			// aapt resource value: 0x7f07009a
-			public const int icon_group = 2131165338;
+			// aapt resource value: 0x7f07009c
+			public const int icon_group = 2131165340;
 			
 			// aapt resource value: 0x7f070025
 			public const int ifRoom = 2131165221;
@@ -2453,23 +2462,23 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070046
 			public const int image = 2131165254;
 			
-			// aapt resource value: 0x7f070096
-			public const int info = 2131165334;
+			// aapt resource value: 0x7f070098
+			public const int info = 2131165336;
 			
 			// aapt resource value: 0x7f070000
 			public const int item_touch_helper_previous_elevation = 2131165184;
 			
-			// aapt resource value: 0x7f07007a
-			public const int largeLabel = 2131165306;
+			// aapt resource value: 0x7f07007c
+			public const int largeLabel = 2131165308;
 			
 			// aapt resource value: 0x7f070034
 			public const int left = 2131165236;
 			
-			// aapt resource value: 0x7f07009b
-			public const int line1 = 2131165339;
-			
 			// aapt resource value: 0x7f07009d
-			public const int line3 = 2131165341;
+			public const int line1 = 2131165341;
+			
+			// aapt resource value: 0x7f07009f
+			public const int line3 = 2131165343;
 			
 			// aapt resource value: 0x7f07000f
 			public const int listMode = 2131165199;
@@ -2477,11 +2486,11 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070048
 			public const int list_item = 2131165256;
 			
-			// aapt resource value: 0x7f0700a1
-			public const int masked = 2131165345;
+			// aapt resource value: 0x7f0700a3
+			public const int masked = 2131165347;
 			
-			// aapt resource value: 0x7f07008f
-			public const int media_actions = 2131165327;
+			// aapt resource value: 0x7f070091
+			public const int media_actions = 2131165329;
 			
 			// aapt resource value: 0x7f070022
 			public const int middle = 2131165218;
@@ -2492,8 +2501,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070019
 			public const int multiply = 2131165209;
 			
-			// aapt resource value: 0x7f070081
-			public const int navigation_header_container = 2131165313;
+			// aapt resource value: 0x7f070083
+			public const int navigation_header_container = 2131165315;
 			
 			// aapt resource value: 0x7f070026
 			public const int never = 2131165222;
@@ -2504,14 +2513,14 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070010
 			public const int normal = 2131165200;
 			
-			// aapt resource value: 0x7f070098
-			public const int notification_background = 2131165336;
+			// aapt resource value: 0x7f07009a
+			public const int notification_background = 2131165338;
 			
-			// aapt resource value: 0x7f070092
-			public const int notification_main_column = 2131165330;
+			// aapt resource value: 0x7f070094
+			public const int notification_main_column = 2131165332;
 			
-			// aapt resource value: 0x7f070091
-			public const int notification_main_column_container = 2131165329;
+			// aapt resource value: 0x7f070093
+			public const int notification_main_column_container = 2131165331;
 			
 			// aapt resource value: 0x7f070037
 			public const int parallax = 2131165239;
@@ -2534,14 +2543,14 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070035
 			public const int right = 2131165237;
 			
-			// aapt resource value: 0x7f070097
-			public const int right_icon = 2131165335;
+			// aapt resource value: 0x7f070099
+			public const int right_icon = 2131165337;
 			
-			// aapt resource value: 0x7f070093
-			public const int right_side = 2131165331;
+			// aapt resource value: 0x7f070095
+			public const int right_side = 2131165333;
 			
-			// aapt resource value: 0x7f070077
-			public const int rv_belege = 2131165303;
+			// aapt resource value: 0x7f070079
+			public const int rv_belege = 2131165305;
 			
 			// aapt resource value: 0x7f07001a
 			public const int screen = 2131165210;
@@ -2606,14 +2615,14 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070017
 			public const int showTitle = 2131165207;
 			
-			// aapt resource value: 0x7f070079
-			public const int smallLabel = 2131165305;
+			// aapt resource value: 0x7f07007b
+			public const int smallLabel = 2131165307;
 			
-			// aapt resource value: 0x7f070080
-			public const int snackbar_action = 2131165312;
+			// aapt resource value: 0x7f070082
+			public const int snackbar_action = 2131165314;
 			
-			// aapt resource value: 0x7f07007f
-			public const int snackbar_text = 2131165311;
+			// aapt resource value: 0x7f070081
+			public const int snackbar_text = 2131165313;
 			
 			// aapt resource value: 0x7f07002e
 			public const int snap = 2131165230;
@@ -2636,8 +2645,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070036
 			public const int start = 2131165238;
 			
-			// aapt resource value: 0x7f07008e
-			public const int status_bar_latest_event_content = 2131165326;
+			// aapt resource value: 0x7f070090
+			public const int status_bar_latest_event_content = 2131165328;
 			
 			// aapt resource value: 0x7f07005e
 			public const int submenuarrow = 2131165278;
@@ -2645,17 +2654,17 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f07006f
 			public const int submit_area = 2131165295;
 			
-			// aapt resource value: 0x7f070076
-			public const int swipe_refresh = 2131165302;
+			// aapt resource value: 0x7f070078
+			public const int swipe_refresh = 2131165304;
 			
 			// aapt resource value: 0x7f070011
 			public const int tabMode = 2131165201;
 			
-			// aapt resource value: 0x7f07009e
-			public const int text = 2131165342;
+			// aapt resource value: 0x7f0700a0
+			public const int text = 2131165344;
 			
-			// aapt resource value: 0x7f07009c
-			public const int text2 = 2131165340;
+			// aapt resource value: 0x7f07009e
+			public const int text2 = 2131165342;
 			
 			// aapt resource value: 0x7f070052
 			public const int textSpacerNoButtons = 2131165266;
@@ -2663,8 +2672,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070051
 			public const int textSpacerNoTitle = 2131165265;
 			
-			// aapt resource value: 0x7f070086
-			public const int text_input_password_toggle = 2131165318;
+			// aapt resource value: 0x7f070088
+			public const int text_input_password_toggle = 2131165320;
 			
 			// aapt resource value: 0x7f07000c
 			public const int textinput_counter = 2131165196;
@@ -2672,8 +2681,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f07000d
 			public const int textinput_error = 2131165197;
 			
-			// aapt resource value: 0x7f070094
-			public const int time = 2131165332;
+			// aapt resource value: 0x7f070096
+			public const int time = 2131165334;
 			
 			// aapt resource value: 0x7f07004a
 			public const int title = 2131165258;
@@ -2690,8 +2699,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f070056
 			public const int topPanel = 2131165270;
 			
-			// aapt resource value: 0x7f07007d
-			public const int touch_outside = 2131165309;
+			// aapt resource value: 0x7f07007f
+			public const int touch_outside = 2131165311;
 			
 			// aapt resource value: 0x7f07000a
 			public const int transition_current_scene = 2131165194;
@@ -2708,8 +2717,8 @@ namespace ReisekostenNative.Droid
 			// aapt resource value: 0x7f07000e
 			public const int view_offset_helper = 2131165198;
 			
-			// aapt resource value: 0x7f0700a0
-			public const int visible = 2131165344;
+			// aapt resource value: 0x7f0700a2
+			public const int visible = 2131165346;
 			
 			// aapt resource value: 0x7f070027
 			public const int withText = 2131165223;
