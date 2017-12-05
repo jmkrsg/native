b4db3fc13b0163f5409fafe095d7f3358931a59d
jmkrsg <josef.mader-kreiner@r-software.at>
2017-12-05 14:41:32 +0100

    Implemented Serviceurlconfiguration

--- ReisekostenNative/ReisekostenNative/ReisekostenNative/Services/ConfigurationService.cs
+++ ReisekostenNative/ReisekostenNative/ReisekostenNative/Services/ConfigurationService.cs
@@ -8,5 +8,26 @@ namespace ReisekostenNative.Services
 {
     class ConfigurationService
     {
+        private static ConfigurationService instance;
+
+        public static ConfigurationService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ConfigurationService();
+                }
+                return instance;
+            }
+        }
+
+        public String BelegserviceURL
+        {
+            get
+            {
+                return "52.169.65.115:8080";
+            }
+        }
     }
 }
