a7a1cdb32e5c393c7d0a90a01023c5ef2a281cc3
Dominik Doppler <dominik.doppler@r-software.at>
2017-12-05 17:54:44 +0100

    ios Screens

--- ReisekostenNative/ReisekostenNative/ReisekostenNative.iOS/BelegNeuTableViewController.cs
+++ ReisekostenNative/ReisekostenNative/ReisekostenNative.iOS/BelegNeuTableViewController.cs
@@ -7,9 +7,9 @@ using UIKit;
 
 namespace ReisekostenNative.iOS
 {
-    public partial class BelegeNeuTableViewController : UIViewController
+    public partial class BelegNeuTableViewController : UITableViewController
 	{
-        public BelegeNeuTableViewController (IntPtr handle) : base (handle)
+        public BelegNeuTableViewController (IntPtr handle) : base (handle)
 		{
 		}
 	}
